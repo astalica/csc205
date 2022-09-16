2309d40f13e216be62150a354c2707e04b0b1014
astalica <113649396+astalica@users.noreply.github.com>
2022-09-15 22:38:26 -0400

    Create AbstractShape.java

---
+++ AbstractShape.java
@@ -0,0 +1,20 @@
+package Lab03;
+
+public abstract class AbstractShape implements TwoDShape,ThreeDShape {
+
+	protected static int numShapesCreated = 0;
+
+	protected void incrementNumShapesCreated() {
+		numShapesCreated++;
+	}
+
+	static int getNumShapesCreated() {
+		return numShapesCreated;
+	}
+
+	public abstract double area();
+	public abstract double perimeter();
+	public abstract double volume();
+		
+		
+}
